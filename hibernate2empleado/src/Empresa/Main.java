08b9f6ea494e3df26ba81222efbbc3edaa43438a
28huugoo <hugogomezjim@gmail.com>
2024-11-13 10:36:19 +0100

    Actu hibernate

--- hibernate2empleado/src/Empresa/Main.java
+++ hibernate2empleado/src/Empresa/Main.java
@@ -5,6 +5,12 @@
  */
 package Empresa;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import static java.time.Clock.system;
+import java.util.Date;
+import org.hibernate.*;
+
 /**
  *
  * @author FP
@@ -14,8 +20,24 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParseException {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session sesion = sessionFactory.openSession();
+        Transaction tx = sesion.beginTransaction();
+
+        //Departamentos d = new Departamentos((byte) 62, "MARKETING", "GUADALAJARA");
+        Departamentos d = new Departamentos((byte) 62);
+        String formatofecha = "1992/08/23";
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy/MM/dd");
+        Date fecha = formatoFecha.parse(formatofecha);
+        //java.sql.Date formatoFechaSQL = new java.sql.Date(fecha.getTime());
+        Empleados e = new Empleados((short) 4455, d, "GALLARDO", "VENDEDOR", (short) 7499, fecha, (float) 1500, (float) 100);
+        //sesion.save(d);
+        sesion.save(e);
+        tx.commit();
+        System.out.println("Registro insertado");
+        sesion.close();
+        sessionFactory.close();
     }
-    
+
 }
